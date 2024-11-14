92a7bea5c6a00c50108e4e289f530af819a8b22d
unknown <hungluu111103@gmail.com>
2024-11-14 23:14:45 +0700

    add post gre config

--- mcq_be_project/mcq_be_project/settings.py
+++ mcq_be_project/mcq_be_project/settings.py
@@ -87,11 +87,14 @@ WSGI_APPLICATION = 'mcq_be_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mcq_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Hung111103!',
+        'HOST': 'localhost',  # or your PostgreSQL server's IP address
+        'PORT': '5432',       # default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
